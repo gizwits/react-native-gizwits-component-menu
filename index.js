cb9c1435903d23a3ba2747be67ba87ab1bb05e57
王锐彬 <492960423@qq.com>
2019-04-04 16:37:25 +0800

    增加 activeOpacity

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ export default class Menu extends Component {
           }}
         >
           <View style={styles.container} onLayout={this._onLayout}>
-            <TouchableHighlight style={styles.maskView} onPress={maskClick}>
+            <TouchableHighlight style={styles.maskView} activeOpacity={0} onPress={maskClick}>
               <SafeAreaView style={{ flex: 1 }}>
                 <View style={{ ...styles.content, height: line * 100, width: contentWidth }}>
                   <Image source={arrow} style={{ width: 15, height: 15, right: 8, top: -13, position: 'absolute' }} />
